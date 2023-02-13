9837a125e518e7244d97690d367da2dde325c6a7
mahdi chihi <mahdichihi95@gmail.com>
2023-02-13 18:02:30 +0100

    Portfolio Items from Array using map method

---
+++ src/components/data/portfolioData.js
@@ -0,0 +1,51 @@
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+export const portfolioData = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Crypto Currency Dashboard & Financial Visualization",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Figma dashboard UI kit for data design web apps",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Maintaining tasks and tracking progress",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Charts templates & infographics in Figma",
+    github: "https://github.com",
+    demo: "https://dribbble.com/shots/20579260-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
+  },
+];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+
+import { portfolioData } from "../data/portfolioData.js";
 
 const Portfolio = () => {
   return (
@@ -14,124 +10,27 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://github.com/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/" className="btn">
-              Github
-            </a>
-            <a
-              href="https://dribbble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {portfolioData.map((portfolio) => (
+          <article key={portfolio.id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={portfolio.image} alt="" />
+            </div>
+            <h3>{portfolio.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={portfolio.github} className="btn">
+                Github
+              </a>
+              <a
+                href={portfolio.demo}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
