a22758c9f616b7e0469d294e319b6c20e454851f
mahdi chihi <mahdichihi95@gmail.com>
2023-02-13 11:56:06 +0100

    Header & Nav Components : * Header Component CSS. * Header Component Responsive Design. * Floating Nav Component JSX. * Floating Nav Component CSS. * Managing Nav States using useState Hook.

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,17 +8,13 @@ const HeaderSocials = () => {
     <div className="header__socials">
       {/* adding the rel attribute to the a elements with the value of "noreferrer noopener" 
       to avoid the security risk and adding content to the a elements for accessibility purposes */}
-      <a
-        href="https://www.linkedin.com/"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
+      <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer ">
         <BsLinkedin />
       </a>
-      <a href="https://github.com/" target="_blank" rel="noreferrer noopener">
+      <a href="https://github.com/" target="_blank" rel="noreferrer ">
         <FaGithub />
       </a>
-      <a href="https://dribbble.com/" target="_blank" rel="noreferrer noopener">
+      <a href="https://dribbble.com/" target="_blank" rel="noreferrer ">
         <FiDribbble />
       </a>
     </div>
