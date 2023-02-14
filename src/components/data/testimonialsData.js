63b89259e63bd9caebf47858226dc390f4d2d1c8
mahdi chihi <mahdichihi95@gmail.com>
2023-02-14 12:07:37 +0100

    Testimonials Component * JSX * CSS * Responsive Design * Generating Testimonials from Array using map method * Creating Carousel/Slides using SwiperJS

---
+++ src/components/data/testimonialsData.js
@@ -0,0 +1,31 @@
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+
+export const testimonialsData = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos optio neque temporibus eaque doloremque a veniam maxime perferendis ipsa voluptatem pariatur voluptates odio qui fuga, saepe debitis tempore nemo non.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Shatta Wale",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos optio neque temporibus eaque doloremque a veniam maxime perferendis ipsa voluptatem pariatur voluptates odio qui fuga, saepe debitis tempore nemo non.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos optio neque temporibus eaque doloremque a veniam maxime perferendis ipsa voluptatem pariatur voluptates odio qui fuga, saepe debitis tempore nemo non.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Nana Ama McBrown",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos optio neque temporibus eaque doloremque a veniam maxime perferendis ipsa voluptatem pariatur voluptates odio qui fuga, saepe debitis tempore nemo non.",
+  },
+];
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import "./Testimonials.css";
+import { testimonialsData } from "../data/testimonialsData.js";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        className="container testimonials__container"
+      >
+        {testimonialsData.map((testimonial, i) => (
+          <SwiperSlide key={i} className="testimonial">
+            <div className="client__avatar">
+              <img src={testimonial.avatar} alt="Avatar One" />
+            </div>
+            <h5 className="client__name">{testimonial.name}</h5>
+            <small className="client__review">{testimonial.review}</small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
