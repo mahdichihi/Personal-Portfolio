6cb099d85c6ec44fe0ce93e0df419e916ae09c76
mahdi chihi <mahdichihi95@gmail.com>
2023-02-15 14:19:26 +0100

    CSS Styles Update 1.1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -34,8 +34,8 @@ const About = () => {
 
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
-              <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <h5>Projects</h5>
+              <small>+80 Completed</small>
             </article>
           </div>
 
